8944d888326e8b40352befa3bdf0751efe3655e6
PeterNeubauer <peterneubauer2@gmail.com>
2013-01-23 16:15:21 +0100

    fixed genmatrix; removed debug comments

--- mpi/project1/stencil.c
+++ mpi/project1/stencil.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <mpi.h>
 #include <errno.h>
+#include <stdarg.h>
 #include "util/args.h"
 
 #ifndef ATYPE
@@ -16,26 +17,18 @@
 
 static int index_to_rank(int r, int c, int i, int j);
 static void rank_to_index(int r, int c, int rank, int* i, int* j);
-static void genmatrix(ATYPE* submat, int x, int y, int w, int h);
+static void genmatrix(ATYPE* submat, int x, int y, int w, int h, int n);
 static void stencil(ATYPE* source, ATYPE* dest, int w, int h);
 
 static void send_row(ATYPE* mat, int w, int y, int peer);
-static void send_col(ATYPE* mat, int h, int x, int peer);
+static void send_col(ATYPE* mat, int w, int h, int x, int peer);
 static void recv_buf(ATYPE* buf, int len, int peer);
+static void buf_to_row(ATYPE* mat, int w, int y, ATYPE* buf);
+static void buf_to_col(ATYPE* mat, int w, int h, int x, ATYPE* buf);
 
 static void* xmalloc(int size);
-static void print_matrix(const char* caption, ATYPE mat[], int m, int n);
-
-/*
-static int *m_ptr, *n_ptr, *c_ptr, *r_ptr, *w_ptr, *h_ptr, *i_ptr, *j_ptr, *x_ptr, *y_ptr;
-
-static void print_vars()
-{
-	fprintf(stdout, "**** m=%d n=%d c=%d r=%d w=%d h=%d i=%d j=%d x=%d y=%d\n",
-		*m_ptr, *n_ptr, *c_ptr, *r_ptr, *w_ptr, *h_ptr, *i_ptr, *j_ptr, *x_ptr, *y_ptr);
-}
-*/
-
+static void print_matrix(const char* caption, ATYPE* mat, int m, int n);
+static void fail(const char* format, ...);
 
 int main(int argc, char* argv[])
 {
@@ -62,20 +55,14 @@ int main(int argc, char* argv[])
 	char name[MPI_MAX_PROCESSOR_NAME];
 	int nlen;
 
-/*
-	m_ptr = &m; n_ptr = &n; c_ptr = &c; r_ptr = &r; w_ptr = &w; h_ptr = &h;
-	i_ptr = &i; j_ptr = &j, x_ptr = &x; y_ptr = &y;
-*/
-
 	if (parse_args(argc, argv, &debug_flag, "rows", &m, "columns", &n, "number of block columns", &c, NULL, NULL) != 0)
 	{
-		exit(1);
+		fail("Error parsing command args.\n");
 	}
 
 	if ((m < 1) || (n < 1) || (c < 1))
 	{
-		fprintf(stderr, "%s\n", "bad input?");
-		exit(1);
+		fail("Bad input?.\n");
 	}
 
 	MPI_Init (&argc, &argv);
@@ -90,26 +77,22 @@ int main(int argc, char* argv[])
 
 	if (r < 1) 
 	{
-		fprintf(stderr, "%s\n", "bad input?");
-		exit(1);
+		fail("Bad input?.\n");
 	}
 
 	if (r * c != p)
 	{
-		fprintf(stderr, "ERROR: %d rows * %d columns cannot be exactly fit to %d nodes.\n", r, c, p);
-		exit(1);
+		fail("ERROR: %d rows * %d columns cannot be exactly fit to %d nodes.\n", r, c, p);
 	}
 
 	if (r > m)
 	{
-		fprintf(stderr, "ERROR: matrix with %d rows cannot be divided into %d rows.\n", m, r);
-		exit(1);
+		fail("ERROR: matrix with %d rows cannot be divided into %d rows.\n", m, r);
 	}
 
 	if (c > n)
 	{
-		fprintf(stderr, "ERROR: matrix with %d columns cannot be divided into %d columns.\n", n, c);
-		exit(1);
+		fail("ERROR: matrix with %d columns cannot be divided into %d columns.\n", n, c);
 	}
 
 	// width and height of a block
@@ -118,12 +101,9 @@ int main(int argc, char* argv[])
 
 	rank_to_index(r, c, rank, &i, &j);
 
-	// fprintf(stderr, "DEBUG: node %d: my block index is i:%d j:%d\n", rank, i, j);
-
 	if ((i < 0) || (j < 0))
 	{
-		fprintf(stderr, "ERROR: this node (%d) does not have an assigned matrix block.\n", rank);
-		exit(1);
+		fail("ERROR: this node (%d) does not have an assigned matrix block.\n", rank);
 	}
 
 	// start indices of this node's submatrix
@@ -131,53 +111,11 @@ int main(int argc, char* argv[])
 	y = i * h;
 
 	// allocate my submatrix (twice)
-	ATYPE* source = xmalloc(w * h * sizeof(ATYPE));
-	genmatrix(source, x, y, w, h);
+	ATYPE* source = xmalloc((w+2) * (h+2) * sizeof(ATYPE));
+	genmatrix(source, x, y, w, h, n);
 	ATYPE* dest = xmalloc(w * h * sizeof(ATYPE));
 
 	/* ================ BEGIN OF ALGORITHM ================ */
-
-/*
-	if (debug_flag)
-	{
-		fprintf(stdout, "DEBUG: node %d: m=%d, n=%d\n", rank, m, n);
-		fprintf(stdout, "DEBUG: node %d: r=%d, c=%d\n", rank, r, c);
-		fprintf(stdout, "DEBUG: node %d: h=%d, w=%d\n", rank, h, w);
-		fprintf(stdout, "DEBUG: node %d: i=%d, j=%d\n", rank, i, j);
-		fprintf(stdout, "DEBUG: node %d: y=%d, x=%d\n", rank, y, x);
-
-		fprintf(stdout, "DEBUG: node %d: submat before stencil\n", rank);
-		for (int ii = 0; ii < h; ii++) 
-		{
-			printf("[%02d]  ", ii);
-			print_vars();
-
-			for (int jj = 0; jj < w; jj++)
-			{
-				printf("%2d  ", source[ii*w+jj]);
-			}
-			printf("\n");
-		}
-		fprintf(stdout, "DEBUG CHECKPOINT\n", rank);
-	}
-*/
-
-	stencil(source, dest, w, h);
-
-/*
-	if (debug_flag)
-	{
-		fprintf(stdout, "DEBUG: node %d: submat after stencil\n", rank);
-		for (int ii = 0; ii < h; ii++) 
-		{
-			for (int jj = 0; jj < w; jj++)
-			{
-				printf("%2d  ", dest[ii*w+jj]);
-			}
-			printf("\n");
-		}
-	}
-*/
 
 	ATYPE* buffer;
 	int ret;
@@ -189,36 +127,24 @@ int main(int argc, char* argv[])
 	{
 		int peer = rank-1;
 
-		fprintf(stderr, "DEBUG: node %d: recv from %d\n", rank, peer);
-
 		recv_buf(buffer, h, peer);
-
-		fprintf(stderr, "DEBUG: node %d: send to %d\n", rank, peer);
-
-		send_col(source, h, 0, peer);
-
-		// TODO: computation
+		send_col(source, w, h, 0, peer);
+		buf_to_col(source, w, h, -1, buffer);
 	}
 	else
 	{
 		int peer = rank+1;
 
 		if (i != c-1)
-		{ 
-			fprintf(stderr, "DEBUG: node %d: send to %d\n", rank, peer);
-
-			send_col(source, h, w-1, peer);
-
-			fprintf(stderr, "DEBUG: node %d: recv from %d\n", rank, peer);
-
+		{
+			send_col(source, w, h, w-1, peer);
 			recv_buf(buffer, h, peer);
+			buf_to_col(source, w, h, w, buffer);
 		}
 		else
 		{
 			memset(buffer, 0, h * sizeof(ATYPE));
 		}
-
-		// TODO: computation
 	}
 
 	free(buffer);
@@ -230,17 +156,16 @@ int main(int argc, char* argv[])
 
 	// rows stage 2: exchange between nodes (2*k-1) <--> (2*k) [row index]
 
-/*
-	if (rank==0) {
-		printf("Rank %d initializing, total %d\n",rank,size);
-	} else {
-		MPI_Recv(&prev,1,MPI_INT,rank-1,HELLO,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		printf("Rank %d on %s received from %d, passing on\n",rank,name,prev);
+
+	// ACTION!
+
+	if (debug_flag && (0 == rank))
+	{
+		print_matrix("Source", source, w+2, h+2);
 	}
-	if (rank+1<size) 
-		{
-			MPI_Send(&rank,1,MPI_INT,rank+1,HELLO,MPI_COMM_WORLD);
-*/
+
+	stencil(source, dest, w, h);
+
 
 	/* ========== OUTPUT =========== */
 
@@ -269,15 +194,8 @@ int main(int argc, char* argv[])
 					matrix[(yy+pi*h)*n+(xx+pj*w)] = dest[yy*w+xx];
 				}
 			}
-
-			for (int ii = 0; ii < m; ii++) 
-			{
-				for (int jj = 0; jj < n; jj++)
-				{
-					printf("%d  ", matrix[ii*n+jj]);
-				}
-				printf("\n");
-			}
+			
+			print_matrix(NULL, matrix, m, n);
 
 			printf("DONE\n");
 		}
@@ -314,39 +232,55 @@ static void rank_to_index(int r, int c, int rank, int* i, int* j)
 	*j = rank % c;
 }
 
-static void genmatrix(ATYPE* submat, int x, int y, int w, int h)
+static void genmatrix(ATYPE* submat, int x, int y, int w, int h, int n)
 {
-	for (int yy = 0; yy < h; yy++)
-	for (int xx = 0; xx < w; xx++)
+	// NOTE: the submatrix has a margin of 1 element on every side
+	//       to hold the elements to be communicated from neighbour
+	//       nodes. genmatrix() temporarily zeroes them out.
+	for (int yy = 0; yy < h+2; yy++)
+	for (int xx = 0; xx < w+2; xx++)
 	{
-		long ii = (yy + y) * w + (xx + x);
-		submat[ii] = (ATYPE) ((13L * ii * ii + ii * 17L + 4L) % 51L);
+		long ii = yy * (w+2) + xx;
+		long seq = (y+yy-1) * n + (x+xx-1);
+
+		if ((0 == xx) || (0 == yy) || (h+1 == xx) || (w+1 == y))
+		{
+			submat[ii] = 0;
+		}
+		else
+		{
+			submat[ii] = (ATYPE) ((13L * seq * seq + seq * 17L + 4L) % 51L);
+		}
 	}
 }
 
 static void stencil(ATYPE* source, ATYPE* dest, int w, int h)
 {
-	for (int y = 1; y < h-1; y++)
-	for (int x = 1; x < w-1; x++)
+	// NOTE: source is 2 rows and 2 columns larger than dest. re-center!
+	for (int y = 0; y < h; y++)
+	for (int x = 0; x < w; x++)
 	{
-		int i = y * w + x;
-		dest[i] = (source[i-1] + source[i+1] + source[i-w] + source[i+w]) / 4;
+		int d = y * w + x;
+		int s = (y+1) * (w+2) + (x+1);
+		dest[d] = (source[s-1] + source[s+1] + source[s-(w+2)] + source[s+(w+2)]) / 4;
 	}
 }
 
 static void send_row(ATYPE* mat, int w, int y, int peer)
 {
-	ATYPE* buf = &mat[y*w];
+	// NOTE: mind the 1-column, 1-row margin on mat
+	ATYPE* buf = &mat[(y+1)*(w+2)+1];
 	int ret = MPI_Send(buf, w, ATYPE_MPI, peer, 0, MPI_COMM_WORLD);
 }
 
-static void send_col(ATYPE* mat, int h, int x, int peer)
+static void send_col(ATYPE* mat, int w, int h, int x, int peer)
 {
+	// NOTE: mind the 1-column, 1-row margin on mat
 	ATYPE* buf = xmalloc(h * sizeof(ATYPE));
 	
 	for (int y = 0; y < h; y++)
 	{
-		buf[y] = mat[y*h+x];
+		buf[y] = mat[(y+1)*(w+2)+x+1];
 	}
 
 	int ret = MPI_Send(buf, h, ATYPE_MPI, peer, 0, MPI_COMM_WORLD);
@@ -359,13 +293,55 @@ static void recv_buf(ATYPE* buf, int len, int peer)
 	int ret = MPI_Recv(buf, len, ATYPE_MPI, peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 }
 
+static void buf_to_row(ATYPE* mat, int w, int y, ATYPE* buf)
+{
+	// NOTE: y here is still the row index of the block, but as
+	//       it will likely point to the margin rows of the 'source'
+	//       matrix, the values of -1 and h are legitimate.
+	ATYPE* begin = &mat[(y+1)*(w+2)+1];
+	memcpy(begin, buf, w * sizeof(ATYPE));
+}
+
+static void buf_to_col(ATYPE* mat, int w, int h, int x, ATYPE* buf)
+{
+	// NOTE: x here is still the column index of the block, but as
+	//       it will likely point to the margin rows of the 'source'
+	//       matrix, the values of -1 and w are legitimate.
+	for (int y = 0; y < h; y++)
+	{
+		mat[(y+1)*(w+2)+x+1] = buf[y];
+	}
+}
+
 static void* xmalloc(int size)
 {
 	void* ret = malloc(size);
 	if (NULL == ret)
 	{
-		fprintf(stderr, "MALLOC ERROR: %s\n", strerror(errno));
-		exit(1);
+		fail("MALLOC ERROR: %s\n", strerror(errno));
 	}
 	else return ret;
+}
+
+static void print_matrix(const char* caption, ATYPE* mat, int m, int n)
+{
+	if (NULL != caption) printf("%s:\n", caption);
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%3d  ", mat[i*n+j]);
+		}
+		printf("\n");
+	}
+}
+
+static void fail(const char* format, ...)
+{
+	va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+	exit(1);
 }
