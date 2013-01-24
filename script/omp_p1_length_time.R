cb2d4dc292d33beecdf748a5dc4ed287b9d8dd99
Bruno <bruno.pfeiffer@gmail.com>
2013-01-24 16:31:36 +0100

    Added R files & scripts

---
+++ script/omp_p1_length_time.R
@@ -0,0 +1,8 @@
+args <- commandArgs(trailingOnly=TRUE)
+rawdata <- read.csv(args[1], header = FALSE, sep=";")
+names(rawdata) <- c('length', 'threads', 'ops', 'time')
+data <- subset(rawdata, threads == args[2], select=c(length, threads, time))
+jpeg(args[3])
+plot(data$length, data$time, main=paste(args[1], ",\nThreads:",args[2], sep=""), xlab="Length", ylab="Time")
+axis(1, at=1:length(data$time), labels=c(data$time))
+dev.off()
---
+++ script/omp_p1_thread_time.R
@@ -0,0 +1,8 @@
+args <- commandArgs(trailingOnly=TRUE)
+rawdata <- read.csv(args[1], header = FALSE, sep=";")
+names(rawdata) <- c('length', 'threads', 'ops', 'time')
+data <- subset(rawdata, length == args[2], select=c(length, threads, time))
+jpeg(args[3])
+plot(data$threads, data$time, main=paste(args[1], ":",args[2], sep=""), xlab="Threads", ylab="Time")
+axis(1, at=1:length(data$threads), labels=c(data$threads))
+dev.off()
