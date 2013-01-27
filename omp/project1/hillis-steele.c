16ce2c7c8bfe2f12ed7387ead962c2007b98a093
PeterNeubauer <peterneubauer2@gmail.com>
2013-01-27 15:44:15 +0100

    performance counters array access for omp project 1

--- omp/project1/hillis-steele.c
+++ omp/project1/hillis-steele.c
@@ -16,15 +16,15 @@
 #endif
 
 static ATYPE* geninput(int n);
-static void benchmark(ATYPE** data, int n, int* ops, double* dtime);
-static void scan(ATYPE** in, int n, int* ops);
+static void benchmark(ATYPE** data, int n, int* plus_ops, int* acc_ops, double* dtime);
+static void scan(ATYPE** in, int n, int* plus_ops, int* acc_ops);
 static ATYPE* reference(int n);
 
 static void* xmalloc(int size);
 static void print_array(const char* caption, ATYPE a[], int n);
 static int array_equal(ATYPE* a1, ATYPE* a2, int n);
-static void print_perf(int n, int t, int ops, double dtime);
-static void print_perf_debug(int n, int t, int ops, double dtime);
+static void print_perf(int n, int t, int plus_ops, int acc_ops, double dtime);
+static void print_perf_debug(int n, int t, int plus_ops, int acc_ops, double dtime);
 static void fail(const char* format, ...);
 
 int main(int argc, char* argv[])
@@ -46,19 +46,20 @@ int main(int argc, char* argv[])
 	omp_set_num_threads(t);
 
 	ATYPE* data = geninput(n);
-	int ops = 0;
+	int plus_ops = 0;
+	int acc_ops = 0;
 
 	if (debug_flag) print_array("input", data, n);
 
 	double dtime;
-	benchmark(&data, n, &ops, &dtime);
+	benchmark(&data, n, &plus_ops, &acc_ops, &dtime);
 
 	if (debug_flag)
 	{
 		/* ========== OUTPUT =========== */
 
 		print_array("output", data, n);
-		print_perf_debug(n, t, ops, dtime);
+		print_perf_debug(n, t, plus_ops, acc_ops, dtime);
 
 		ATYPE* ref = reference(n);
 
@@ -75,9 +76,9 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
-		print_perf(n, t, ops, dtime);
+		print_perf_debug(n, t, plus_ops, acc_ops, dtime);
 	}
-	
+
 	free(data);
 }
 
@@ -96,45 +97,52 @@ static ATYPE* geninput(int n)
 	return a;
 }
 
-static void benchmark(ATYPE** data, int n, int* ops, double* dtime)
+static void benchmark(ATYPE** data, int n, int* plus_ops, int* acc_ops, double* dtime)
 {
 	double stime = omp_get_wtime();
-	scan(data, n, ops);
+	scan(data, n, plus_ops, acc_ops);
 	double etime = omp_get_wtime();
 
 	*dtime = etime - stime;
 }
 
-static void scan(ATYPE** in, int n, int* ops)
+static void scan(ATYPE** in, int n, int* plus_ops, int* acc_ops)
 {
 	int i;
 	int k;
 	ATYPE* a;
 	ATYPE* b;
 	ATYPE* temp;
-	int pc_ops;
+	int p_ops;
+	int a_ops;
 
 	a = *in;
 	b = malloc(sizeof(ATYPE) * n);
 
 	for (k = 1; k < n; k<<=1) 
 	{
-		pc_ops = 0;
+		p_ops = 0;
+		a_ops = 0;
 
-		#pragma omp parallel for
+		#pragma omp parallel for reduction(+: a_ops)
 		for (i = 0; i < k; i++)
 		{
 			b[i] = a[i];
+			a_ops+=2;
 		}
+		*acc_ops += a_ops;
+		a_ops = 0;
 
-		#pragma omp parallel for reduction(+: pc_ops)
+		#pragma omp parallel for reduction(+: p_ops) reduction(+: a_ops)
 		for (i = k; i < n; i++)
 		{
 			b[i] = a[i] + a[i-k];
-			pc_ops++;
+			p_ops++;
+			a_ops+=3;
 		}
 
-		*ops += pc_ops;
+		*plus_ops += p_ops;
+		*acc_ops += a_ops;
 
 		// swappity
 		temp = a;
@@ -190,16 +198,17 @@ static int array_equal(ATYPE* a1, ATYPE* a2, int n)
 	return 1;
 }
 
-static void print_perf(int n, int t, int ops, double dtime)
+static void print_perf(int n, int t, int plus_ops, int acc_ops, double dtime)
 {
-	printf("%d%s%d%s%d%s%f\n", n, SEP, t, SEP, ops, SEP, dtime);
+	printf("%d%s%d%s%d%s%d%s%f\n", n, SEP, t, SEP, plus_ops, SEP, acc_ops, SEP, dtime);
 }
 
-static void print_perf_debug(int n, int t, int ops, double dtime)
+static void print_perf_debug(int n, int t, int plus_ops, int acc_ops, double dtime)
 {
 	printf("Input size: %d\n", n);
 	printf("Threads: %d\n", t);
-	printf("Total operations: %d\n", ops);
+	printf("Plus operations: %d\n", plus_ops);
+	printf("Access operations: %d\n", acc_ops);
 	printf("Time: %f\n", dtime);
 }
 
--- omp/project1/recursive.c
+++ omp/project1/recursive.c
@@ -18,15 +18,15 @@
 // TODO: Additional performance counters for array access (in addition to addition counters)
 
 static ATYPE* geninput(int n);
-static void benchmark(ATYPE data[], int n, int* ops, double* dtime);
-static void scan(ATYPE a[], int n, int* ops);
+static void benchmark(ATYPE data[], int n, int* plus_ops, int* acc_ops, double* dtime);
+static void scan(ATYPE a[], int n, int* plus_ops, int* acc_ops);
 static ATYPE* reference(int n);
 
 static void* xmalloc(int size);
 static void print_array(const char* caption, ATYPE a[], int n);
 static int array_equal(ATYPE* a1, ATYPE* a2, int n);
-static void print_perf(int n, int t, int ops, double dtime);
-static void print_perf_debug(int n, int t, int ops, double dtime);
+static void print_perf(int n, int t, int plus_ops, int acc_ops, double dtime);
+static void print_perf_debug(int n, int t, int plus_ops, int acc_ops, double dtime);
 static void fail(const char* format, ...);
 
 int main(int argc, char* argv[])
@@ -48,19 +48,20 @@ int main(int argc, char* argv[])
 	omp_set_num_threads(t);
 
 	ATYPE* data = geninput(n);
-	int ops = 0;
+	int plus_ops = 0;
+	int acc_ops = 0;
 
 	if (debug_flag) print_array("input", data, n);
 
 	double dtime;
-	benchmark(data, n, &ops, &dtime);
+	benchmark(data, n, &plus_ops, &acc_ops, &dtime);
 
 	if (debug_flag)
 	{
 		/* ========== OUTPUT =========== */
 
 		print_array("output", data, n);
-		print_perf_debug(n, t, ops, dtime);
+		print_perf_debug(n, t, plus_ops, acc_ops, dtime);
 
 		ATYPE* ref = reference(n);
 
@@ -77,7 +78,7 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
-		print_perf(n, t, ops, dtime);
+		print_perf(n, t, plus_ops, acc_ops, dtime);
 	}
 
 	free(data);
@@ -98,53 +99,61 @@ static ATYPE* geninput(int n)
 	return a;
 }
 
-static void benchmark(ATYPE data[], int n, int* ops, double* dtime)
+static void benchmark(ATYPE data[], int n, int* plus_ops, int* acc_ops, double* dtime)
 {
 	double stime = omp_get_wtime();
-	scan(data, n, ops);
+	scan(data, n, plus_ops, acc_ops);
 	double etime = omp_get_wtime();
 
 	*dtime = etime - stime;
 }
 
-static void scan(ATYPE a[], int n, int* ops)
+static void scan(ATYPE a[], int n, int* plus_ops, int* acc_ops)
 {
 	int i;
 	ATYPE* b;
-	int pc_ops;
+	int p_ops;
+	int a_ops;
 
 	if (n <= 1) return;
 
 	b = alloca(sizeof(ATYPE) * (n/2));
-	pc_ops = 0;
+	p_ops = 0;
+	a_ops = 0;
 
-	#pragma omp parallel for reduction(+: pc_ops)
+	#pragma omp parallel for reduction(+: p_ops) reduction(+: a_ops)
 	for (i = 0; i < n/2; i++) 
 	{
 		b[i] = a[2*i] + a[2*i+1];
-		pc_ops++;
+		p_ops++;
+		a_ops+=3;
 	}
 
-	*ops += pc_ops;
+	*plus_ops += p_ops;
+	*acc_ops += a_ops;
 
-	scan(b, n/2, ops);
+	scan(b, n/2, plus_ops, acc_ops);
 
 	a[1] = b[0];
-	pc_ops = 0;
+	p_ops = 0;
+	a_ops = 0;
 
-	#pragma omp parallel for reduction(+: pc_ops)
+	#pragma omp parallel for reduction(+: p_ops) reduction(+: a_ops)
 	for (i = 1; i < n/2; i++) {
 		a[2*i] = b[i-1] + a[2*i];
 		a[2*i+1] = b[i];
-		pc_ops++;
+		p_ops++;
+		a_ops+=5;
 	}
 
-	*ops += pc_ops;
+	*plus_ops += p_ops;
+	*acc_ops += a_ops;
 
 	if (n % 2)
 	{
 		a[n-1] = b[n/2-1] + a[n-1];
-		(*ops)++;
+		(*plus_ops)++;
+		(*acc_ops)+=3;
 	}
 }
 
@@ -193,16 +202,17 @@ static int array_equal(ATYPE* a1, ATYPE* a2, int n)
 	return 1;
 }
 
-static void print_perf(int n, int t, int ops, double dtime)
+static void print_perf(int n, int t, int plus_ops, int acc_ops, double dtime)
 {
-	printf("%d%s%d%s%d%s%f\n", n, SEP, t, SEP, ops, SEP, dtime);
+	printf("%d%s%d%s%d%s%d%s%f\n", n, SEP, t, SEP, plus_ops, SEP, acc_ops, SEP, dtime);
 }
 
-static void print_perf_debug(int n, int t, int ops, double dtime)
+static void print_perf_debug(int n, int t, int plus_ops, int acc_ops, double dtime)
 {
 	printf("Input size: %d\n", n);
 	printf("Threads: %d\n", t);
-	printf("Total operations: %d\n", ops);
+	printf("Plus operations: %d\n", plus_ops);
+	printf("Access operations: %d\n", acc_ops);
 	printf("Time: %f\n", dtime);
 }
 
